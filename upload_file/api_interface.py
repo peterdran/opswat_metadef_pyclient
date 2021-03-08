6fda5b1d0d628b6c8205ec4c2cd5f057a4f8a4b3
Peter Dranishnikov <pdranishnikov5258@floridapoly.edu>
2021-03-08 18:25:32 -0500

    Add basic methods for api interface

---
+++ upload_file/api_interface.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def metadef_api_lookup_by_hash(hash_str, api_key):
+    api_url_str = "https://api.metadefender.com/v4/"
+    headers = {
+    'apikey': api_key
+    }
+
+def metadef_api_upload_file(file_content_bin, file_name_str, api_key):
+    api_url_str = "https://api.metadefender.com/v4/"
+    headers = {
+    'apikey': api_key
+    }
+
+def metadef_api_query_file_results(file_id_str, api_key):
+    
+    api_url_str = "https://api.metadefender.com/v4/file/" + file_id_str #TODO use template method to prevent code execution
+    #file id for a test? bzIxMDMwNi00VS0wY2VFV3ZhaUc4Nnk5TkU
+    
+    headers = {
+    'apikey': api_key #do not fill out! must read from the cli or a file...else brain damage occurs and have to start over
+    }
+    #TODO try..get this statement
+    response = requests.request("GET", api_url_str, headers=headers) #generic response, modify to suit needs
+    
+
+def metadef_api_progress_poll(file_id_str):
+    api_url_str = "https://api.metadefender.com/v4/"
+    headers = {
+    'apikey': api_key
+    }
+    
+    #TODO try..get this statement
+    response = requests.request("GET", api_url_str, headers=headers)
+    
--- upload_file/upload_file.py
+++ upload_file/upload_file.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 import sys
 
+import file_helper
+import api_interface
+
+def result_formatter(json_obj):
+    
+
 if __name__ == '__main__':
     #(pre-task) handle args
+    #(pre-task) get api key somehow
     arg_name_str = #TODO whatever the arg thingy is
     #open file
-    input_file_bin = open_arbitrary_file(arg_name_str)
+    input_file_bin = file_helper.open_arbitrary_file(arg_name_str)
     
     #compute hash
-    file_sha256_str = hash_file(arg_name_str)
+    file_sha256_str = file_helper.hash_file(arg_name_str)
     #lookup hash via API
     
     #logic fork: if found show results via id else
